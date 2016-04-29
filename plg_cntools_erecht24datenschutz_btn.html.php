4f68a29c19f4f7d106c942e34ead94027e87e80c
cn-tools <cn-tools@gmx.at>
2016-04-29 06:31:48 +0200

    add youtube, ...

--- plg_cntools_erecht24datenschutz_btn.html.php
+++ plg_cntools_erecht24datenschutz_btn.html.php
@@ -87,34 +87,38 @@ function doAddContent($params, $name, $label, $desc) {
                 function insertParameter(editor)
                 {
 					var lValue = "";
+					
 					if (document.erecht24_parameter.standard[0].checked == true) { lValue = lValue + "&standard=1"; }
 					if (document.erecht24_parameter.privacy[0].checked == true) { lValue = lValue + "&privacy=1"; }
-					if (document.erecht24_parameter.facebook[0].checked == true) { lValue = lValue + "&facebook=1"; }
+					if (document.erecht24_parameter.cookies[0].checked == true) { lValue = lValue + "&cookies=1"; }
+					if (document.erecht24_parameter.serverlogfiles[0].checked == true) { lValue = lValue + "&serverlogfiles=1"; }
+					if (document.erecht24_parameter.contactform[0].checked == true) { lValue = lValue + "&contactform=1"; }
+					if (document.erecht24_parameter.newsletter[0].checked == true) { lValue = lValue + "&newsletter=1"; }
 					if (document.erecht24_parameter.analytics[0].checked == true) { lValue = lValue + "&analytics=1"; }
-					if (document.erecht24_parameter.plusone[0].checked == true) { lValue = lValue + "&plusone=1"; }
-					//if (document.erecht24_parameter.adsense[0].checked == true) { lValue = lValue + "&adsense=1"; }
-					//if (document.erecht24_parameter.remarketing[0].checked == true) { lValue = lValue + "&remarketing=1"; }
 					if (document.erecht24_parameter.etracker[0].checked == true) { lValue = lValue + "&etracker=1"; }
+					if (document.erecht24_parameter.piwik[0].checked == true) { lValue = lValue + "&piwik=1"; }
+					if (document.erecht24_parameter.facebook[0].checked == true) { lValue = lValue + "&facebook=1"; }
 					if (document.erecht24_parameter.twitter[0].checked == true) { lValue = lValue + "&twitter=1"; }
-					if (document.erecht24_parameter.xing[0].checked == true) { lValue = lValue + "&xing=1"; }
+					if (document.erecht24_parameter.plusone[0].checked == true) { lValue = lValue + "&plusone=1"; }
 					if (document.erecht24_parameter.instagram[0].checked == true) { lValue = lValue + "&instagram=1"; }
 					if (document.erecht24_parameter.linkedin[0].checked == true) { lValue = lValue + "&linkedin=1"; }
 					if (document.erecht24_parameter.pinterest[0].checked == true) { lValue = lValue + "&pinterest=1"; }
+					if (document.erecht24_parameter.xing[0].checked == true) { lValue = lValue + "&xing=1"; }
 					if (document.erecht24_parameter.tumblr[0].checked == true) { lValue = lValue + "&tumblr=1"; }
+					if (document.erecht24_parameter.youtube[0].checked == true) { lValue = lValue + "&youtube=1"; }
+					//if (document.erecht24_parameter.adsense[0].checked == true) { lValue = lValue + "&adsense=1"; }
+					//if (document.erecht24_parameter.remarketing[0].checked == true) { lValue = lValue + "&remarketing=1"; }
 					//if (document.erecht24_parameter.amazon[0].checked == true) { lValue = lValue + "&amazon=1"; }
-					if (document.erecht24_parameter.infodelete[0].checked == true) { lValue = lValue + "&infodelete=1"; }
-					if (document.erecht24_parameter.serverlogfiles[0].checked == true) { lValue = lValue + "&serverlogfiles=1"; }
-					if (document.erecht24_parameter.cookies[0].checked == true) { lValue = lValue + "&cookies=1"; }
-					if (document.erecht24_parameter.contactform[0].checked == true) { lValue = lValue + "&contactform=1"; }
-					if (document.erecht24_parameter.advertemail[0].checked == true) { lValue = lValue + "&advertemail=1"; }
-					if (document.erecht24_parameter.newsletter[0].checked == true) { lValue = lValue + "&newsletter=1"; }
 					//if (document.erecht24_parameter.registration[0].checked == true) { lValue = lValue + "&registration=1"; }
+					//if (document.erecht24_parameter.dataprocessing[0].checked == true) { lValue = lValue + "&dataprocessing=1"; }
 					//if (document.erecht24_parameter.shops[0].checked == true) { lValue = lValue + "&shops=1"; }
 					//if (document.erecht24_parameter.onlinecontracts[0].checked == true) { lValue = lValue + "&onlinecontracts=1"; }
-					//if (document.erecht24_parameter.dataprocessing[0].checked == true) { lValue = lValue + "&dataprocessing=1"; }
+					if (document.erecht24_parameter.infodelete[0].checked == true) { lValue = lValue + "&infodelete=1"; }
+					if (document.erecht24_parameter.advertemails[0].checked == true) { lValue = lValue + "&advertemails=1"; }
+					//if (document.erecht24_parameter.translation_en[0].checked == true) { lValue = lValue + "&translation_en=1"; }
+
 					if (lValue!=""){
 						lValue = "{ERecht24Datenschutz}" + lValue + "{/ERecht24Datenschutz}";
-						//alert("Variable: "+lValue+"!"); //Testausgabe 
 						window.parent.jInsertEditorText(lValue, '<?php echo preg_replace('#[^A-Z0-9\-\_\[\]]#i', '', $name); ?>');
 					}
 					window.parent.SqueezeBox.close();
@@ -135,29 +139,32 @@ function doAddContent($params, $name, $label, $desc) {
 							<?php
 							doAddContent($params, 'standard', 'Disclaimer', '');
 							doAddContent($params, 'privacy', 'Datenschutzerklärung', '');
-							doAddContent($params, 'facebook', 'Facebook', '');
+							doAddContent($params, 'cookies', 'Cookies', '');
+							doAddContent($params, 'serverlogfiles', 'Server-Log-Dateien', '');
+							doAddContent($params, 'contactform', 'Kontaktformular', '');
+							doAddContent($params, 'newsletter', 'Newsletter', '');
 							doAddContent($params, 'analytics', 'Google Analytics', '');
-							doAddContent($params, 'plusone', 'Google +1', '');
-							//doAddContent($params, 'adsense', 'Google Adsense', '');
-							//doAddContent($params, 'remarketing', 'Google Analytics Remarketing', '');
 							doAddContent($params, 'etracker', 'etracker', '');
+							doAddContent($params, 'piwik', 'Piwik', '');
+							doAddContent($params, 'facebook', 'Facebook', '');
 							doAddContent($params, 'twitter', 'Twitter', '');
-							doAddContent($params, 'xing', 'Xing', '');
+							doAddContent($params, 'plusone', 'Google +1', '');
 							doAddContent($params, 'instagram', 'Instagram', '');
 							doAddContent($params, 'linkedin', 'Linkedin', '');
 							doAddContent($params, 'pinterest', 'Pinterest', '');
+							doAddContent($params, 'xing', 'Xing', '');
 							doAddContent($params, 'tumblr', 'Tumblr', '');
-							//doAddContent($params, 'amazon', 'Amazon Partnerprogramm', '');
+							doAddContent($params, 'youtube', 'YouTube', '');
+//							doAddContent($params, 'adsense', 'Google Adsense', '');
+//							doAddContent($params, 'remarketing', 'Google Analytics Remarketing', '');
+//							doAddContent($params, 'amazon', 'Amazon Partnerprogramm', '');
+//							doAddContent($params, 'registration', 'Nutzerregistrierung', '');
+//							doAddContent($params, 'dataprocessing', 'Datenschutzerklärung zum Verarbeiten von Kunden- und Vertragsdaten', '');
+//							doAddContent($params, 'shops', 'Datenübermittlung - Online-Shops & Händler (mit Warenversand)', '');
+//							doAddContent($params, 'onlinecontracts', 'Datenübermittlung - Dienstleister, die online Verträge schließen (ohne Warenversand)', '');
 							doAddContent($params, 'infodelete', 'Auskunft, Löschung, Sperrung', '');
-							doAddContent($params, 'serverlogfiles', 'Server-Log-Dateien', '');
-							doAddContent($params, 'cookies', 'Cookies', '');
-							doAddContent($params, 'contactform', 'Kontaktformular', '');
-							doAddContent($params, 'advertemail', 'Werbe-E-Mails', '');
-							doAddContent($params, 'newsletter', 'Newsletter', '');
-							//doAddContent($params, 'registration', 'Nutzerregistrierung', '');
-							//doAddContent($params, 'shops', 'Datenübermittlung - Online-Shops & Händler (mit Warenversand)', '');
-							//doAddContent($params, 'onlinecontracts', 'Datenübermittlung - Dienstleister, die online Verträge schließen (ohne Warenversand)', '');
-							//doAddContent($params, 'dataprocessing', 'Datenschutzerklärung zum Verarbeiten von Kunden- und Vertragsdaten', '');
+							doAddContent($params, 'advertemails', 'Werbe-E-Mails', '');
+//							doAddContent($params, 'translation_en', '??????????', '');
 							?>
                         </tbody>
                     </table>
